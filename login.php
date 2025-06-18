3233f83489f22f990877e6f3e7032defd8c4c14f
LuisKlt <luiseduardofdl2003@gmail.com>
2025-06-18 00:14:02 -0300

    Trabalho finalizado

--- login.php
+++ login.php
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/styles.css">
 </head>
-<body class="bg-dark">
+<body class="tela-login bg-dark">
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-5">
